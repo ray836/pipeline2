a030b0ab94a3b73effa2721b4f1f4bd3e1331419
Ray Grant <raykyleg@amazon.com>
2023-03-11 17:59:06 -0700

    define initale pipeline

--- lib/aws-cdk-pip-stack.ts
+++ lib/aws-cdk-pip-stack.ts
@@ -1,16 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkPipStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkPipQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: "DemoPipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub("ray836/pipeline2", "main"),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
   }
 }
